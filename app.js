b8e071eb3402268d5bec488fb2b680d68826e708
AntonellaAnnecchini <136344437+AntonellaAnnecchini@users.noreply.github.com>
2025-12-03 08:29:11 -0300

    diciembre

---
+++ app.js
@@ -0,0 +1,21 @@
+// objeto/*
+
+let estudiantes = [
+    {
+
+        Año: '3',
+        Nombre: 'Sofia',
+        Apellido: 'Flores',
+        Nota: 7
+
+
+    },
+    {
+        Año: '4',
+        Nombre: 'Malena',
+        Apellido: 'Quinteros',
+        Nota: 9
+    }
+]
+let boton = document.querySelector('#estudiantes')
+boton.onclick
